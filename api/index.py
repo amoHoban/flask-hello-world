427b7102f02b66f00bc34f516cf49ff02ec52386
ahoban <ahmed.hoban@nttdata.com>
2023-01-28 12:23:52 +0100

    add endpoint

--- api/index.py
+++ api/index.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, jsonify
+import gdown
 
 app = Flask(__name__)
 
@@ -9,3 +10,9 @@ def home():
 @app.route('/about')
 def about():
     return 'About'
+
+
+@app.route('/run-colab')
+def run_colab():
+    gdown.download('https://drive.google.com/file/d/1NkBYI8SYXTvvdj6Cxjg-gGdVE-hDugjn#scrollTo=gXaoZs2lh1hi', 'colab.ipynb', quiet=False)
+    return jsonify(message='colab notebook ran successfully')
